7d85a3d91a366b604122c2ac31c145269dd5a77f
Saad Raza <saadraza.official@gmail.com>
2023-05-28 19:34:29 +0500

    Rust learning day 1-2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::io;
 fn main() {
-    println!("Hello, world!");
+
+    let mut input = String::new();
+    take_distance(&mut input);
+    println!("distance from {} ",input);
+     calculate_distance_from_world(&mut input);
+    println!("distance from {} ",input);
+    println!("distance from {} ",&input);
+    println!("distance from {} ",&mut input);
+}
+
+
+    fn calculate_distance_from_world( distance :&mut String) -> String {
+        println!("Number1: {}", distance);
+       
+        let parsed_distance: i64 = distance.trim().parse().expect("Failed to calculate distance from input");
+    
+        println!("number {} ",parsed_distance);
+        let calculated_distance = (parsed_distance * 100).to_string();
+        distance.clear();
+        distance.push_str(&calculated_distance);
+      
+        return calculated_distance;
+        
+    }
+fn take_distance(distance_input : &mut String) {
+    io::stdin().read_line(distance_input).expect("Failed to read distance from input");
+
 }
